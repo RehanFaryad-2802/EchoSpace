e9b1130214627df44b9f57c9cf3a612df924f2f8
RehanFaryad-2802 <seoneoptmizer@gmail.com>
2024-11-28 15:48:49 +0500

    Completed

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { Stats } = require("fs");
 const { userInfo } = require("os");
 const path = require("path");
+var methodOverride = require('method-override')
 const { v4: uuidv4 } = require("uuid");
 const app = express();
 const port = 8080;
 
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -25,7 +27,9 @@ let posts = [
     gender: "male",
     id: uuidv4(),
     content: `Coding has become a cornerstone of the modern world, driving innovation and powering technology. It allows individuals to communicate with computers and create programs, websites, apps, and more. Learning to code is not just for tech professionals; it is a valuable skill for anyone looking to thrive in today's digital landscape.`,
-    imgageURL: `https://picsum.photos/200/100?random=${Math.floor(Math.random()*9999+1)}`,
+    imgageURL: `https://picsum.photos/200/100?random=${Math.floor(
+      Math.random() * 9999 + 1
+    )}`,
   },
   {
     username: "Ammar",
@@ -34,13 +38,17 @@ let posts = [
 
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, magnam? Dolorum reprehenderit quia deserunt in commodi eum explicabo? Tenetur, animi.",
-      imgageURL: `https://picsum.photos/200/100?random=${Math.floor(Math.random()*9999+1)}`,
+    imgageURL: `https://picsum.photos/200/100?random=${Math.floor(
+      Math.random() * 9999 + 1
+    )}`,
   },
   {
     username: "Ying",
     gender: "female",
     id: uuidv4(),
-    imgageURL: `https://picsum.photos/200/100?random=${Math.floor(Math.random()*9999+1)}`,
+    imgageURL: `https://picsum.photos/200/100?random=${Math.floor(
+      Math.random() * 9999 + 1
+    )}`,
 
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, magnam? Dolorum reprehenderit quia deserunt in commodi eum explicabo? Tenetur, animi.",
@@ -49,7 +57,9 @@ let posts = [
     username: "Ling ",
     gender: "female",
     id: uuidv4(),
-    imgageURL: `https://picsum.photos/200/100?random=${Math.floor(Math.random()*9999+1)}`,
+    imgageURL: `https://picsum.photos/200/100?random=${Math.floor(
+      Math.random() * 9999 + 1
+    )}`,
 
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, magnam? Dolorum reprehenderit quia deserunt in commodi eum explicabo? Tenetur, animi.",
@@ -64,25 +74,40 @@ app.get("/posts", (req, res) => {
 app.get("/posts/:id", (req, res) => {
   let { id } = req.params;
   let post = posts.find((post) => id === post.id);
+  // console.log(post)
   res.render("single-post.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((post) => id === post.id);
+  post.content = newContent;
+  res.redirect('/posts')
 });
 
 app.get("/posts/:id/edit", (req, res) => {
   let { id } = req.params;
   let post = posts.find((post) => id === post.id);
-  res.render('edit.ejs')
-  
+  res.render("edit.ejs", { post});
+});
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((post) => id !== post.id);
+  res.redirect('/posts')
 });
 
-// app.patch("/posts/:id", (req, res) => {
-//   let { id } = req.params;
-//   let post = posts.find((post) => id === post.id);
-//   let newContent = req.body.content
-//   post.content = newContent;
-// });
 
 app.post("/posts/new", (req, res) => {
   let { username, gender, content } = req.body;
-  posts.unshift({ username, gender, id: uuidv4(), content, imgageURL: `https://picsum.photos/200/100?random=${Math.floor(Math.random()*9999+1)}` });
+  posts.unshift({
+    username,
+    gender,
+    id: uuidv4(),
+    content,
+    imgageURL: `https://picsum.photos/200/100?random=${Math.floor(
+      Math.random() * 9999 + 1
+    )}`,
+  });
   res.redirect("/posts");
 });
