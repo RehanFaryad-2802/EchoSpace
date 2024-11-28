ff7c92989f67684df94f4a4808875a84f5c3248e
RehanFaryad-2802 <seoneoptmizer@gmail.com>
2024-11-28 08:39:03 +0500

    Posts page created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { Stats } = require("fs");
+const { userInfo } = require("os");
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = 8080;
 
@@ -17,3 +19,45 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
+let posts = [
+  {
+
+    userInfo: {
+      username: "Rehan Faryad",
+      gender: 'male',
+      id: uuidv4()
+    },
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, magnam? Dolorum reprehenderit quia deserunt in commodi eum explicabo? Tenetur, animi.",
+  },
+  {
+
+    userInfo: {
+      username: "Ammar",
+      gender: 'male',
+      id: uuidv4()
+    },
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, magnam? Dolorum reprehenderit quia deserunt in commodi eum explicabo? Tenetur, animi.",
+  },
+  {
+
+    userInfo: {
+      username: "Ying",
+      gender: 'female',
+      id: uuidv4()
+    },
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, magnam? Dolorum reprehenderit quia deserunt in commodi eum explicabo? Tenetur, animi.",
+  },
+  {
+
+    userInfo: {
+      username: "Ling ",
+      gender: 'female',
+      id: uuidv4()
+    },
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, magnam? Dolorum reprehenderit quia deserunt in commodi eum explicabo? Tenetur, animi.",
+  },
+]
+app.get("/posts", (req, res) => {
+  res.render("posts.ejs",{posts});
+});
+
